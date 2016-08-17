517c37d34e1759d3ad81c4acb36d1b0040f23847
Tim Lebel <tim.lebel@gmail.com>
2016-08-16 21:38:18 -0800

    Tests and travis

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,11 @@
 using JVM,
-      Base.Test
+      FactCheck
 
-@test !JVM.isgit("Compat")
-@test !JVM.isgit("AppConf")
-@test JVM.isgit("git@github.com:tmlbl/AppConf.jl")
-@test JVM.isgit("https://github.com/tmlbl/AppConf.jl")
-@test JVM.namefromgit("git@github.com:tmlbl/AppConf.jl.git") == "AppConf"
-@test JVM.namefromgit("https://github.com/tmlbl/AppConf.jl") == "AppConf"
+facts("git utils") do
+  @fact !JVM.isgit("Compat") --> true
+  @fact !JVM.isgit("AppConf") --> true
+  @fact JVM.isgit("git@github.com:tmlbl/AppConf.jl") --> true
+  @fact JVM.isgit("https://github.com/tmlbl/AppConf.jl") --> true
+  @fact JVM.namefromgit("git@github.com:tmlbl/AppConf.jl.git") --> "AppConf"
+  @fact JVM.namefromgit("https://github.com/tmlbl/AppConf.jl") --> "AppConf" 
+end
